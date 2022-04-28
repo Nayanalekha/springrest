5377d8108a8d5a8e37245e19a3cca7d3794e37e2
Nayanalekha <38463625+Nayanalekha@users.noreply.github.com>
2022-04-29 10:40:41 +1200

    first commit tested with postman

---
+++ src/main/java/com/ns/springrest/Customers.java
@@ -0,0 +1,48 @@
+package com.ns.springrest;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Customers {
+    @Id
+    @GeneratedValue
+    int id;
+    String name;
+    String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Customers{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ src/main/resources/sample.sql
@@ -0,0 +1,4 @@
+INSERT INTO CUSTOMERS VALUES ("1", gaya, gaya@gmail.com);
+INSERT INTO CUSTOMERS VALUES ("2", haya, haya@gmail.com);
+INSERT INTO CUSTOMERS VALUES ("3", jaya, jaya@gmail.com);
+INSERT INTO CUSTOMERS VALUES ("4", kaya, kaya@gmail.com);
